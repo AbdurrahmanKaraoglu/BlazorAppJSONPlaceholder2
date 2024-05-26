959b47c62a871bd69d4567ecca9ccb46abc8428b
Abdurrahman Karaoğlu <a_karaoglu@hotmail.com>
2024-05-26 22:43:26 +0300

    asa

--- BlazorAppJSONPlaceholder/Program.cs
+++ BlazorAppJSONPlaceholder/Program.cs
@@ -1,10 +1,16 @@
 using BlazorAppJSONPlaceholder.Components;
+using BlazorAppJSONPlaceholder.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<UserService>();
+builder.Services.AddSingleton<AlbumService>();
+builder.Services.AddSingleton<PhotoService>();
+
 
 var app = builder.Build();
 
